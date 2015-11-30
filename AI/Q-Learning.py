a8657afd47440695892b58e1c7d8b01a68e2432c
Max Robinson <maxrgit@gmail.com>
2015-11-29 23:11:07 -0800

    Compressing the state works and is unit tested.

--- AI/Q-Learning.py
+++ AI/Q-Learning.py
@@ -115,8 +115,7 @@ class AIPlayer(Player):
     # consolidateState
     #   Given a state, consolidates the state to only needed information.
     # the information needed in the consolidated state is as follows:
-    #   - a generalized location for each ant (none queen), ( puts the ants on a 5x5 board instead of a 10 by 10 )
-    #   - a generalized location for each queen
+    #   - a generalized location for each ant (including queen), ( puts the ants on a 5x5 board instead of a 10 by 10 )
     #   - a generalized location for each ant hill and tunnel
     #   - a boolean of if my queen is on the ant hill or not.
     #       - The queen could be on the " same " generalized location as the ant hill but not ACTUALLY be on the
@@ -127,8 +126,8 @@ class AIPlayer(Player):
     #
     ##
     def consolidateState(self, currentState):
-        myInventory = currentState.inventory[self.playerId]
-        enemyInventory = currentState.inventory[(self.playerId + 1)%2]
+        myInventory = currentState.inventories[self.playerId]
+        enemyInventory = currentState.inventories[(self.playerId + 1)%2]
 
         # create compressed state
         compressedState = state()
@@ -152,34 +151,42 @@ class AIPlayer(Player):
         for coords in myAntCoords:
             compressedState.antPositionList.append(coords)
 
-        #EnemyAntCoords
+        # EnemyAntCoords
         enemyAntCoords = [self.generalizeCoords(ant.coords) for ant in enemyInventory.ants]
 
         # add coords to ant positions
         for coords in enemyAntCoords:
             compressedState.antPositionList.append(coords)
 
-
-        ## food
+        # food
         compressedState.myFoodCount = myInventory.foodCount
         compressedState.enemyFoodCount = enemyInventory.foodCount
 
+        # check if anything is on my HILL location
+        compressedState.anythingOnHill = self.anythingOnHill(myInventory, enemyInventory)
 
-        hillLocation = myInventory.getAnthill.coords()
-        ## check if anything is on my HILL location
-        for coord in [x.coords for ant in myInventory.ants]:
+
+        # return the fully compressed state.
+        return compressedState
+
+
+
+    ## TODO
+    def anythingOnHill(self, myInventory, enemyInventory):
+        hillLocation = myInventory.getAnthill().coords
+        onHillValue = False
+        for coord in [ant.coords for ant in myInventory.ants]:
             if coord == hillLocation:
-                compressedState.anythingOnHill = True
+                onHillValue = True
 
         # if something already on it we know.
-        if not compressedState.anythingOnHill:
+        if not onHillValue:
             # otherwise check if enemy ants are on it.
-            for coords in [x.coords for ant in enemyInventory.ants]:
+            for coords in [ant.coords for ant in enemyInventory.ants]:
                 if coords == hillLocation:
-                    compressedState.anythingOnHill = True
+                    onHillValue = True
 
-
-        print("hello world")
+        return onHillValue
 
 
     ##
@@ -198,8 +205,7 @@ class AIPlayer(Player):
 ##
 # class State:
 #  A state has the following attributes.
-#   - a generalized location for each ant (none queen), ( puts the ants on a 5x5 board instead of a 10 by 10 )
-#   - a generalized location for each queen (in ant position list)
+#   - a generalized location for each ant (including queens), ( puts the ants on a 5x5 board instead of a 10 by 10 )
 #   - a generalized location for each ant hill and tunnel
 #   - a boolean of if any ant is on the ant hill or not.
 #       * Any ant could be on the "same" generalized location as the ant hill but not ACTUALLY be on the
@@ -215,10 +221,10 @@ class state:
         self.antPositionList = []
 
         self.myHillLocation = None
-        self.myTunnelLocations = None
+        self.myTunnelLocations = []
 
         self.enemyHillLocation = None
-        self.enemyTunnelLocations = None
+        self.enemyTunnelLocations = []
 
         self.anythingOnHill = False
 
@@ -226,9 +232,51 @@ class state:
         self.enemyFoodCount = 0
 
 
+    def __eq__(self, other):
+        if set(self.antPositionList) == set(other.antPositionList) and \
+            self.myHillLocation == other.myHillLocation and \
+            set(self.myTunnelLocations) == set(other.myTunnelLocations) and \
+            self.enemyHillLocation == other.enemyHillLocation and \
+            set(self.enemyTunnelLocations) == set(other.enemyTunnelLocations) and \
+            self.anythingOnHill == other.anythingOnHill and \
+            self.myFoodCount == other.myFoodCount and \
+            self.enemyFoodCount == other.enemyFoodCount:
+
+            return True
+
+    def __hash__(self):
+        bigList = []
+        bigList.append(self.antPositionList)
+
+        bigList.append(self.myHillLocation)
+        bigList.append(self.myTunnelLocations)
+
+        bigList.append(self.enemyHillLocation)
+        bigList.append(self.enemyTunnelLocations)
+
+        bigList.append(self.anythingOnHill)
+
+        bigList.append(self.myFoodCount)
+        bigList.append(self.enemyFoodCount)
+
+        return hash((tuple(self.antPositionList), self.myHillLocation,
+                     tuple(self.myTunnelLocations), self.enemyHillLocation,
+                     tuple(self.enemyTunnelLocations), self.anythingOnHill,
+                     self.myFoodCount, self.enemyFoodCount))
+
+
+from GameState import *
+from Location import *
+from Inventory import *
+from Construction import *
+from Ant import *
+
 class UnitTests:
+
+
     def __init__(self):
-        self.player = AIPlayer(1)
+        self.player = AIPlayer(PLAYER_ONE)
+        self.state = state()
 
         print("Start Unit Tests: ")
 
@@ -239,8 +287,157 @@ class UnitTests:
         else:
             print("GenCoords: BAD!")
 
+    def testStateEquality(self):
+        if(self.state.__eq__(self.state)):
+            print("State Equality: Equal")
+        else:
+            print("State Equality: Equal")
 
+    def testStateHash(self):
+        print("Init Hash Value: ", self.state.__hash__())
+
+        self.state.enemyFoodCount = 1
+        print("Next Hash Value: ", self.state.__hash__())
+        temp = set()
+        temp.add(self.state)
+        print("Added a state to a set")
+
+
+    def testCompressState(self):
+        print("compressing State")
+        tempState = self.setupState()
+
+
+        compressedState = self.player.consolidateState(tempState)
+
+        expectedCompressedState = state()
+        expectedCompressedState.myHillLocation = (0,1)
+        expectedCompressedState.myTunnelLocations = [(0,1)]
+
+        expectedCompressedState.enemyHillLocation = (0,3)
+        expectedCompressedState.enemyTunnelLocations = [(0,3)]
+
+        expectedCompressedState.antPositionList = [(0,1), (0,3), (0,3)]
+        expectedCompressedState.anythingOnHill = True
+
+        if(compressedState.__eq__(expectedCompressedState)):
+            print("Compression worked!!!!!!")
+        else:
+            print("Compression failed!!!!!!")
+            attrs = vars(expectedCompressedState)
+            print("Expected State: ")
+            print(','.join("%s: %s" % item for item in attrs.items()))
+
+            attrs2 = vars(compressedState)
+            print("Actual State: ")
+            print(','.join("%s: %s" % item for item in attrs2.items()))
+
+
+
+
+    def setupState(self):
+        board = [[Location((col, row)) for row in xrange(0, BOARD_LENGTH)] for col in xrange(0, BOARD_LENGTH)]
+        p1Inventory = Inventory(PLAYER_ONE, [], [], 0)
+        p2Inventory = Inventory(PLAYER_TWO, [], [], 0)
+        neutralInventory = Inventory(NEUTRAL, [], [], 0)
+
+        self.putFood(neutralInventory)
+        self.putOurInventory(p1Inventory)
+        self.putTheirInventory(p2Inventory)
+
+        state = GameState(board, [p1Inventory, p2Inventory, neutralInventory], PLAY_PHASE, PLAYER_ONE)
+
+        return state
+
+    # #
+    # putFood
+    # Description: places the Food for our set up for our gameState for the Unit Test
+    #
+    # Parameters:
+    #   neutralInventory - the inventory where grass and food is placed
+    #
+    # Return: Nothing
+    # #
+    def putFood(self, neutralInventory):
+        for i in range(0,9):
+            ourGrass = Construction((i, 0), GRASS)
+            otherGrass = Construction((i,9), GRASS)
+            neutralInventory.constrs.append(ourGrass)
+            neutralInventory.constrs.append(otherGrass)
+        for i in range(0,2):
+            ourFood = Construction((i,1), FOOD)
+            otherFood = Construction((i,8), FOOD)
+            neutralInventory.constrs.append(ourFood)
+            neutralInventory.constrs.append(otherFood)
+
+
+    # #
+    # putOurInventory
+    # Description: places the ants and anthill for our set up for our AI's inventory for the gameState for the Unit Test
+    #
+    # Parameters:
+    #   inventory - the inventory for our AI.
+    #
+    # Return: Nothing
+    # #
+    def putOurInventory(self, inventory):
+        inventory.constrs.append(Construction((0,3), ANTHILL))
+        inventory.constrs.append(Construction((1,3), TUNNEL))
+        inventory.ants.append(Ant((0,3), QUEEN, PLAYER_ONE)) # Queen
+        inventory.ants.append(Ant((0,6), DRONE, PLAYER_ONE)) # Queen
+
+
+    # #
+    # putTheirInventory
+    # Description: places the ants and anthill for our set up for our opponent's inventory
+    # for the gameState for the Unit Test
+    #
+    # Parameters:
+    #   inventory - the inventory for our opponent.
+    #
+    # Return: Nothing
+    # #
+    def putTheirInventory(self, inventory):
+        inventory.constrs.append(Construction((0, 7), ANTHILL))
+        inventory.constrs.append(Construction((1, 7), TUNNEL))
+        queen = Ant((0, 7), QUEEN, PLAYER_TWO)
+        queen.health = 1
+        inventory.ants.append(queen) # Queen
+
+    # #
+    # equalStates
+    # Description: Checks if two game states are equal
+    #
+    # Parameters:
+    #   state1 - a game state
+    #   state2 - a game state to check against.
+    #
+    # Return: Boolean: True if equal, False if Not
+    # #
+    def equalStates(self, state1, state2):
+        if state1.phase != state2.phase or state1.whoseTurn != state2.whoseTurn:
+            return False
+        for i in range(0,3):
+            state1Inv = state1.inventories[i]
+            state2Inv = state1.inventories[i]
+            if len(state1Inv.constrs) != len(state2Inv.constrs):
+                return False
+            for j in range(0, len(state1Inv.constrs)):
+                if state1Inv.constrs[i] != state1Inv.constrs[i]:
+                    return False
+            if i < 2:
+                if len(state1Inv.ants) != len(state2Inv.ants):
+                    return False
+                for k in range(0, len(state1Inv.ants)):
+                    if state1Inv.ants[i] != state1Inv.ants[i]:
+                        return False
+                if state1Inv.foodCount != state2Inv.foodCount:
+                    return False
+        return True
 
 
 unitTest = UnitTests()
 unitTest.testGeneralizeCoords()
+unitTest.testStateHash()
+unitTest.testStateEquality()
+unitTest.testCompressState()
