971410d6b9fb5c736a4aa7c252658ddb5b35b0fa
Max Robinson <maxrgit@gmail.com>
2015-12-02 01:20:26 -0800

    Initial implimentation of TD-Learning Using Eligibility Trace and GLIE
    As of right now, it looks like it is playing games at pretty slow rate...
    I believe this could be due to a few things, 1) my state space is too big.
    2) certain parts of my code are being inefficient.
    TODO: go through and look for possible areas of inefficiencies.
    
    Also need to put method headers on certain methods.
    TODO: Write Unit tests for untested methods!

--- AI/Q-Learning.py
+++ AI/Q-Learning.py
@@ -11,6 +11,14 @@ from GameState import addCoords
 from AIPlayerUtils import *
 
 
+## Global Constants
+INIT_ET = 1.0
+
+STATE_INDEX = 0
+UTILITY_INDEX = 1
+ET_INDEX = 2
+
+
 ##
 #AIPlayer
 #Description: The responsbility of this class is to interact with the game by
@@ -30,20 +38,33 @@ class AIPlayer(Player):
     ##
     def __init__(self, inputPlayerId):
         super(AIPlayer,self).__init__(inputPlayerId, "TD-Learning")
-        self.memoryFileName = "TD-Learning.txt"
-        self.alphaValueFileName = "TD-AlphaValue.txt"
+        self.memoryFileName = "robinsom16_TD-Learning.txt"
+        self.alphaValueFileName = "robinsom16_TD-AlphaValue.txt"
+        self.epsilonValueFileName = "robinsom16_TD-EpsilonValue.txt"
 
         self.alphaExponentNumber = -0.2
         self.gameNumber = 1
-        self.gama = .8
-        self.alpha = .99
 
-        # key: HASH VALUE of a given state, Value: utility or list of Utility and EligibilityTrace value
+        self.gamma = .8
+        self.alpha = .999
+        self.lambdaValue = .95
+        self.epsilon = .99
+
+        # key: HASH VALUE of a given compressed State,
+        # Value: list [ compressedState, Utility value, EligibilityTrace value]
         self.stateUtilityMemory = {}
 
         # If we have an existing memory, Load it!!! If not don't do anything.
-        if(os.path.isfile("TD-Learning.txt")):
+        if(os.path.isfile("robinsom16_TD-Learning.txt")):
             self.readMemory()
+
+        # If we have an alpha value, Load it!!! If not don't do anything.
+        if(os.path.isfile("robinsom16_TD-AlphaValue.txt")):
+            self.readAlphaValue()
+
+        # If we have an alpha value, Load it!!! If not don't do anything.
+        if(os.path.isfile("robinsom16_TD-EpsilonValue.txt")):
+            self.readEpsilonValue()
 
     ##
     #getPlacement
@@ -100,18 +121,114 @@ class AIPlayer(Player):
             return [(0, 0)]
 
     ##
-    #getMove
-    #Description: Gets the next move from the Player.
+    # getMove
+    # Description:
+    #   First, updates the AI's Memory based on the state it is currently in.
+    #   Then, it gets the next move from the Player, based on possible moves and an expand node fucntion.
     #
-    #Parameters:
+    # Parameters:
     #   currentState - The state of the current game waiting for the player's move (GameState)
     #
-    #Return: The Move to be made
+    # Return:
+    #   move - The Move to be made
     ##
     def getMove(self, currentState):
-        moves = listAllLegalMoves(currentState)
-        return moves[random.randint(0,len(moves) - 1)]
 
+        # compress state
+        compressedState = self.consolidateState(currentState)
+
+        # first: update memory
+        self.updateMemory(compressedState)
+
+        # Second: find the best possible move or explore
+        move = self.getBestMove(currentState)
+
+        return move
+
+
+    ##
+    # getBestMove
+    #   Description:
+    #   Given a current state, this method looks at all of the possible moves that can be made,
+    #   If we role, and we get a number less than epsilon (the explore value) then we choose a random move
+    #   Otherwise, we expand the move and get back what the state would look like if we took that move,
+    #       we then compress this state, and see if that state is in our Memory.
+    #       If it is in our memory, we see if it has the maximum util value of the other reachable states,
+    #           if it is, then we update the maxUtil seen and set the move to return to the move associated with
+    #           getting to that state.
+    #       If it is not in our memory we skip it.
+    #   If at the end, we have not found any states that match in our memory, make a random move.
+    ##
+    def getBestMove(self, currentState):
+
+        # get all possible moves.
+        moves = listAllLegalMoves(currentState)
+        move = None
+
+        role = random.random()
+        # if less than epsilon, explore via random move
+        if(role < self.epsilon):
+            move = moves[random.randint(0,len(moves) - 1)]
+        elif(move == None):
+            maxUtil = 0
+            for posMove in moves:
+                # get the state we'd be in with the move
+                expandState = self.expandNode(currentState, posMove)
+
+                # compress the state after that move
+                compressPosState = self.consolidateState(expandState)
+
+                # get the key into the dictionary for that state
+                key = compressPosState.__hash__()
+
+                #if that state is in the dictionary
+                if key in self.stateUtilityMemory:
+                    # get that states util
+                    keyUtil = self.stateUtilityMemory[key][UTILITY_INDEX]
+
+                    # if it's the best util we've seen set the move equal to that move, and update the maxUtil
+                    if keyUtil > maxUtil:
+                        maxUtil = keyUtil
+                        move = posMove
+
+        # if for some reason the move is still none, get a random move
+        # this could occur if we haven't seen any of the states that the moves could give us.
+        if(move is None):
+            move = moves[random.randint(0, len(moves) - 1)]
+
+        return move
+
+
+    ## TODO
+    def updateMemory(self, compressedState):
+        # if the current state is not in memory, add it to memory, and give the utility the reward value of the state
+        if compressedState.__hash__() not in self.stateUtilityMemory:
+            self.stateUtilityMemory[compressedState.__hash__()] = [compressedState, self.getReward(compressedState), INIT_ET]
+
+        # get the values stored for current state -
+        currentStateUtilAndEt = self.stateUtilityMemory[compressedState.__hash__()]
+
+        # update all states Utilities using Eligibility Trace.
+        for key in self.stateUtilityMemory:
+            utilAndEt = self.stateUtilityMemory[key]
+            state = utilAndEt[STATE_INDEX]
+            stateUtil = utilAndEt[UTILITY_INDEX]
+            stateEt = utilAndEt[ET_INDEX]
+
+            # update the Util Value of all states
+            # equation: where s' is my current state
+            # U(s) = U(s) + alpha(reward(s) + gama(U(s')) - U(s))
+            utilAndEt[UTILITY_INDEX] = stateUtil + stateEt * self.alpha*(self.getReward(state) +
+                                           self.gamma*(currentStateUtilAndEt[UTILITY_INDEX]) - stateUtil )
+
+            # update the ET value of all states
+            utilAndEt[ET_INDEX] = stateEt * self.lambdaValue * self.gamma
+
+        # if we have seen this state before, Reset the ET value
+        if compressedState.__hash__() in self.stateUtilityMemory:
+            currentStateUtilAndEt[ET_INDEX] = 1
+
+        return
     ##
     #getAttack
     #Description: Gets the attack to be made from the Player
@@ -132,7 +249,22 @@ class AIPlayer(Player):
     # Parameter: hasWon - a boolean saying if we have won or not.
     ##
     def registerWin(self, hasWon):
+
+        # create a win or lose state ( based on if we won or lost)
+        winComppressedState = state()
+        winComppressedState.hasWon = hasWon
+        winComppressedState.hasLost = not hasWon
+
+        # use the win or lose state to update the memory
+        self.updateMemory(winComppressedState)
+        self.alpha *= 0.999
+        self.epsilon *= 0.99
+
+        # save info
         self.saveMemory()
+        self.saveAlphaValue()
+        self.saveEpsilonValue()
+
 
     ##
     # consolidateState
@@ -190,6 +322,13 @@ class AIPlayer(Player):
         # check if anything is on my HILL location
         compressedState.anythingOnHill = self.anythingOnHill(myInventory, enemyInventory)
 
+        # check if we have won.
+        compressedState.hasWon = self.hasWon(self.playerId, currentState)
+
+        # check if we have Lost.
+        # giving it the opponents ID.
+        compressedState.hasLost = self.hasWon((self.playerId + 1) % 2, currentState)
+
 
         # return the fully compressed state.
         return compressedState
@@ -237,19 +376,40 @@ class AIPlayer(Player):
 
     ##
     # getReward
+    #   Gives the reward for the AI based on a given state of winning or loosing or if in game.
     #
     # Parameter:
-    #   state - the state that is being examined for the reward.
+    #   gameOver: Boolean of if the game is over
+    #   hasWon: Boolean of if the player has won or not. (should be false if gameOver is false)
     #
     # Return:
     #   rewardValue - the value of the reward based on the state
-    #
     ##
-    def getReward(self, state):
+    # def getReward(self, gameOver, hasWon):
+    #     # +1 for win
+    #     # -1 for loss
+    #     # else: -0.01
+    #     if(gameOver):
+    #         if(hasWon):
+    #             return 1.0
+    #         else:
+    #             return -1.0
+    #     else:
+    #         return -0.01
+
+
+
+    def getReward(self, compressedState):
         # +1 for win
         # -1 for loss
         # else: -0.01
-        print("reward")
+        if(compressedState.hasWon):
+            return 1.0
+        elif(compressedState.hasLost):
+            return -1.0
+        else:
+            return -0.01
+
 
     ##
     # saveMemory
@@ -259,6 +419,24 @@ class AIPlayer(Player):
         outputFile = file(self.memoryFileName, "w")
         pickle.dump(self.stateUtilityMemory, outputFile)
         outputFile.close()
+
+
+    ##
+    # saveAlphaValue
+    #   Saves alpha value to file to a file so that the AI may continue learning after a given number of games.
+    #   (Aka. pick up where it left off).
+    ##
+    def saveAlphaValue(self):
+        outputFile = file(self.alphaValueFileName, "w")
+        outputFile.write(str(self.alpha))
+        outputFile.close()
+
+
+    def saveEpsilonValue(self):
+        outputFile = file(self.epsilonValueFileName, "w")
+        outputFile.write(str(self.epsilon))
+        outputFile.close()
+
 
     ##
     # readMemory
@@ -271,6 +449,211 @@ class AIPlayer(Player):
         self.stateUtilityMemory = pickle.load(inputFile)
         inputFile.close()
 
+
+    ##
+    # readAlphaValue
+    #   Saves alpha value to file to a file so that the AI may continue learning after a given number of games.
+    #   (Aka. pick up where it left off).
+    ##
+    def readAlphaValue(self):
+        inputFile = file(self.alphaValueFileName, "r")
+        self.alpha = float(inputFile.read())
+        inputFile.close()
+
+    ##
+    # readEpsilonValue
+    #   Reads alpha value to file to a file so that the AI may continue learning after a given number of games.
+    #   (Aka. pick up where it left off).
+    ##
+    def readEpsilonValue(self):
+        inputFile = file(self.epsilonValueFileName, "r")
+        self.epsilon = float(inputFile.read())
+        inputFile.close()
+
+    # #
+    # expandNode
+    # Description: This method takes a game state, and a move, and applies the move
+    #   to the game State to depict what the game state would look like after the move is made
+    #
+    # Parameters:
+    #    currentState - A clone of the current state (GameState)
+    #    move - a game move to be executed to expand the state
+    # #
+    def expandNode(self, currentState, move):
+        gameState = currentState.fastclone()
+        ourInventory = gameState.inventories[self.playerId]
+        if(move.moveType == MOVE_ANT):
+            antToMove = None
+            for ant in ourInventory.ants:
+                if ant.coords == move.coordList[0]:
+                    antToMove = ant
+            if antToMove is not None:
+                antToMove.coords = move.coordList[-1]
+                antToMove.hasMoved = True
+
+                # check if other ants near by for attack
+                opponentId = self.getOpponentId()
+                enemyInv = gameState.inventories[opponentId]
+                ## Checks if can attack.
+                self.attackSequence(enemyInv, antToMove)
+
+        elif(move.moveType == BUILD):
+            # just worried about building Ants and Tunnel
+            if(move.buildType == WORKER):
+                # add ant
+                ourInventory.ants.append(Ant(move.coordList[-1], WORKER, self.playerId))
+                # subtract food
+                ourInventory.foodCount -= 1
+            elif(move.buildType == DRONE):
+                ourInventory.ants.append(Ant(move.coordList[-1], DRONE, self.playerId))
+                ourInventory.foodCount -= 1
+            elif(move.buildType == SOLDIER):
+                ourInventory.ants.append(Ant(move.coordList[-1], SOLDIER, self.playerId))
+                ourInventory.foodCount -= 2
+            elif(move.buildType == R_SOLDIER):
+                ourInventory.ants.append(Ant(move.coordList[-1], R_SOLDIER, self.playerId))
+                ourInventory.foodCount -= 2
+            elif(move.buildType == TUNNEL):
+                ourInventory.constrs.append(Building(move.coordList[-1], TUNNEL, self.playerId))
+                ourInventory.foodCount -= 3
+        else:
+            self.pickUpFood(gameState, ourInventory)
+            self.dropOffFood(gameState, ourInventory)
+            return gameState
+
+        return gameState
+
+    # #
+    # attackSequence
+    # Description: This method determines if there are any valid attacks for ourAI, and if so,
+    #   to evaluate the attack by picking a random enemy to attack.
+    #
+    # Parameters:
+    #    enemyInv - The opponents Inventory
+    #    antToMove - The ant that is moving into attack range.
+    #
+    # Return: Nothing
+    # #
+    def attackSequence(self, enemyInv, antToMove ):
+        attackedAntList = []
+        for ant in enemyInv.ants:
+            if self.isValidAttack(antToMove, ant.coords):
+                # keep track of valid ants to attack
+                attackedAntList.append(ant)
+
+        if len(attackedAntList) > 0:
+            antToAttack = attackedAntList[random.randint(0, len(attackedAntList)-1)]
+            # subtract health
+            if antToMove == SOLDIER or antToMove == QUEEN:
+                antToAttack.health -= 2
+            else:
+                antToAttack.health -= 1
+            #if ant dies, remove it from list
+            if antToAttack.health <= 0:
+                enemyInv.ants.remove(antToAttack)
+
+    # #
+    # isValidAttack
+    # Description: Determines if an attack is valid.
+    #   This method was taken from the Game.py file, and slightly modified by current authors.
+    #
+    # Parameters:
+    #    attackingAnt - The ant that will be attacking
+    #    attackCoord - The Coords of where the ant is attacking
+    #
+    # Return: A boolean: True for Valid attack, False otherwise.
+    def isValidAttack(self, attackingAnt, attackCoord):
+        if attackCoord == None:
+            return None
+        # we know we have an enemy ant
+        range = UNIT_STATS[attackingAnt.type][RANGE]
+        diffX = abs(attackingAnt.coords[0] - attackCoord[0])
+        diffY = abs(attackingAnt.coords[1] - attackCoord[1])
+
+        # pythagoras would be proud
+        if range ** 2 >= diffX ** 2 + diffY ** 2:
+            # return True if within range
+            return True
+        else:
+            return False
+
+    # #
+    # getOpponentId
+    # Description: Helper method to get the opponent's ID
+    #
+    # Parameters:
+    #    None
+    #
+    # Return: The opponent's ID
+    # #
+    def getOpponentId(self):
+        opponentId = -1
+        if self.playerId == 0:
+            opponentId = 1
+        else:
+            opponentId = 0
+        return opponentId
+
+    ##
+    # pickUpFood
+    # Description: This method edits the game state to pickUP food if a worker ant that can carry is standing
+    #   on food.
+    #
+    # Parameters:
+    #    gameState - The state being edited.
+    #    ourInventory - our Inventory
+    #
+    # Return: Nothing
+    # #
+    def pickUpFood(self, gameState, ourInventory):
+        # check if food there
+        for ant in ourInventory.ants:
+            if getConstrAt(gameState, ant.coords) is not None:
+                if getConstrAt(gameState, ant.coords).type == FOOD and (not ant.carrying):
+                    ant.carrying = True
+
+    # #
+    # dropOffFood
+    # Description: This method edits the game state to drop off food if a worker ant with food is standing
+    #   on a tunnel or anthill.
+    #
+    # Parameters:
+    #    gameState - The state being edited.
+    #    ourInventory - our Inventory
+    #
+    # Return: Nothing
+    # #
+    def dropOffFood(self, gameState, ourInventory):
+        # check if landded on tunnel or anthill
+        for ant in ourInventory.ants:
+            if getConstrAt(gameState, ant.coords) is not None:
+                if getConstrAt(gameState, ant.coords).type == TUNNEL or \
+                        getConstrAt(gameState, ant.coords).type == ANTHILL\
+                        and ant.carrying:
+                    ant.carrying = False
+                    ourInventory.foodCount += 1
+
+    ##
+    #hasWon(int)
+    #Description: Determines whether the game has ended in victory for the given player.
+    #
+    #Parameters:
+    #   playerId - The ID of the player being checked for winning (int)
+    #
+    #Returns: True if the player with playerId has won the game.
+    ##
+    def hasWon(self, playerId, state):
+        opponentId = (playerId + 1) % 2
+
+        if ((state.phase == PLAY_PHASE) and
+        ((state.inventories[opponentId].getQueen() == None) or
+        # (state.inventories[opponentId].getAnthill().captureHealth <= 0) or
+        (state.inventories[playerId].foodCount >= FOOD_GOAL) or
+        (state.inventories[opponentId].foodCount == 0 and
+            len(state.inventories[opponentId].ants) == 1))):
+            return True
+        else:
+            return False
 
 ##
 # class State:
@@ -301,6 +684,9 @@ class state:
         self.myFoodCount = 0
         self.enemyFoodCount = 0
 
+        self.hasWon = False
+        self.hasLost = False
+
 
     def __eq__(self, other):
         if set(self.antPositionList) == set(other.antPositionList) and \
@@ -310,7 +696,9 @@ class state:
             set(self.enemyTunnelLocations) == set(other.enemyTunnelLocations) and \
             self.anythingOnHill == other.anythingOnHill and \
             self.myFoodCount == other.myFoodCount and \
-            self.enemyFoodCount == other.enemyFoodCount:
+            self.enemyFoodCount == other.enemyFoodCount and \
+            self.hasWon == other.hasWon and \
+            self.hasLost == other.hasLost:
 
             return True
 
@@ -318,7 +706,8 @@ class state:
         return hash((tuple(self.antPositionList), self.myHillLocation,
                      tuple(self.myTunnelLocations), self.enemyHillLocation,
                      tuple(self.enemyTunnelLocations), self.anythingOnHill,
-                     self.myFoodCount, self.enemyFoodCount))
+                     self.myFoodCount, self.enemyFoodCount,
+                     self.hasWon, self.hasLost))
 
 
 from GameState import *
@@ -375,6 +764,8 @@ class UnitTests:
 
         expectedCompressedState.antPositionList = [(0,1), (0,3), (0,3)]
         expectedCompressedState.anythingOnHill = True
+        expectedCompressedState.hasWon = True
+        expectedCompressedState.hasLost = False
 
         if(compressedState.__eq__(expectedCompressedState)):
             print("Compression worked!!!!!!")
